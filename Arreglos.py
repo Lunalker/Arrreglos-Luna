bc13186447c1566f6fc66eb4b8b4bc08a36f728b
Lunalker <deinerquero1@gmail.com>
2025-04-21 13:29:59 -0400

    avance de arreglos

--- Arreglos.py
+++ Arreglos.py
@@ -1,1 +1,10 @@
 #hola soy un comentario
+Frutas = ["manzana", "pera", "naranja"]
+print(Frutas)
+
+
+estudiantes =[
+    { "nombre" : "maxi" , "edad" : 20, "curso" : "2A" } 
+    { "nombre" : "richard", "edad" : 19, "curso" : "2A" }
+]
+print(estudiantes[1])
